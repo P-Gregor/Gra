3abef0e7a519dfb65010cd7461952021503c256c
P-Gregor <ktuury234@gmail.com>
2023-10-10 21:32:21 +0200

    ostatnie na dzisio

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,21 +8,17 @@ using UnityEngine.UIElements;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody player;
-    public float moveSpeed = 25f; // Adjust this value to set the player's maximum movement speed. Default: 5f
-    public float friction = -1000000f;  // Adjust this value to control the rate of speed reduction
-    public float stoppingSpeed = 20f; // Adjust this value to control how quickly the player stops
+    private float moveSpeed = 5f; // Adjust this value to set the player's maximum movement speed. Default: 5f
+    private float stoppingSpeed = 20f; // Adjust this value to control how quickly the player stops
     private float currentSpeed = 0f;
-    // private bool blink = false;
-    // private Vector3 lastMoveDirection;
-    //Both variables used for teleportation
+    private float damping = 1f;
+    private Vector3 moveVelocity;
     private bool dash = false;
-    public Vector3 lastPosition;
+    private Vector3 lastPosition;
     private bool stableGround = true;
-    public float teleportDistance = 5f; // Adjust this value to set the teleport distance
     void Start()
     {
         player.freezeRotation = true;
-        Debug.Log("Players position: " + player.position);
     }
     private void Awake()
     {
@@ -30,7 +26,7 @@ public class PlayerMovement : MonoBehaviour
     }
 
 
-    public void movement()
+    private void movement()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float VerticalInput = Input.GetAxis("Vertical");
@@ -76,37 +72,12 @@ public class PlayerMovement : MonoBehaviour
 
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             dash = true;
         }
-        /*
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-
-        // Calculate the movement direction based on input
-        Vector3 moveDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized;
-
-        // Check if the player is moving
-        if (moveDirection != Vector3.zero)
-        {
-            // Store the last movement direction
-            lastMoveDirection = moveDirection;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            // Calculate the teleport position based on the current position, last move direction, and teleport distance
-            Vector3 teleportPosition = transform.position + lastMoveDirection * teleportDistance;
-
-            // Teleport the player to the new position
-            transform.position = teleportPosition;
-        }
-        //blink but doesnt work as intended. Collision detection needed
-        */
-
     }
 
     private void FixedUpdate()
@@ -131,7 +102,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Wall") 
+        if (other.gameObject.tag == "Wall")
         {
             stableGround = false;
         }
